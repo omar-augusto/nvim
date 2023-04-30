d8718b9f81377458a70015260b3798cb043e6422
Omar Augusto <omaraugusto@bego.ai>
2023-04-29 22:12:31 -0600

    feat: change airline theme

--- after/plugin/airline.rc.lua
+++ after/plugin/airline.rc.lua
@@ -4,4 +4,4 @@ vim.cmd[[let g:airline#extensions#tabline#enabled = 1]]
 vim.cmd[[let g:airline#extensions#tabline#left_alt_sep = '|']]
 vim.cmd[[let g:airline#extensions#tabline#left_sep = ' ']]
 
--- airline theme tomorrow
+vim.cmd[[:AirlineTheme base16_atelier_lakeside]]
