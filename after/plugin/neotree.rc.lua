e1ff4538bd853498f81cbe9c266ea1b3d109e75d
Omar Augusto <omaraugusto@bego.ai>
2023-07-07 12:42:47 -0600

    feat: add document symbols neotree config

--- after/plugin/neotree.rc.lua
+++ after/plugin/neotree.rc.lua
@@ -223,7 +223,27 @@ neotree.setup({
         ["gg"] = "git_commit_and_push",
       }
     }
-  }
+  },
+  document_symbols = {
+    kinds = {
+      File = { icon = "󰈙", hl = "Tag" },
+      Namespace = { icon = "󰌗", hl = "Include" },
+      Package = { icon = "󰏖", hl = "Label" },
+      Class = { icon = "󰌗", hl = "Include" },
+      Property = { icon = "󰆧", hl = "@property" },
+      Enum = { icon = "󰒻", hl = "@number" },
+      Function = { icon = "󰊕", hl = "Function" },
+      String = { icon = "󰀬", hl = "String" },
+      Number = { icon = "󰎠", hl = "Number" },
+      Array = { icon = "󰅪", hl = "Type" },
+      Object = { icon = "󰅩", hl = "Type" },
+      Key = { icon = "󰌋", hl = "" },
+      Struct = { icon = "󰌗", hl = "Type" },
+      Operator = { icon = "󰆕", hl = "Operator" },
+      TypeParameter = { icon = "󰊄", hl = "Type" },
+      StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+    }
+  },
 })
 
 vim.keymap.set('n', '/', ':Neotree toggle current reveal_force_cwd<cr>')
