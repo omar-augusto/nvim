9085d5afbc4dec031b43392c176a46951aad3cd6
Omar Augusto <omaraugusto@bego.ai>
2023-09-11 17:20:07 -0600

    feat: add indent_blankline config

---
+++ after/plugin/indent-blankline.lua
@@ -0,0 +1,16 @@
+local status, neotree = pcall(require, 'indent_blankline')
+
+if not status then
+  print('indent_blankline package is not installed')
+  return
+end
+
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+}
