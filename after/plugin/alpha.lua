113fa6e4d95b285fdfff8060dd2255085461d43c
Omar Augusto <omaraugusto@bego.ai>
2023-10-15 23:58:42 -0600

    feat: add header and mru sections to alpha

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -5,7 +5,14 @@ if not status then
     return
 end
 
-local function footer()
+local a_startify = require('alpha.themes.startify')
+local a_dashboard = require('alpha.themes.dashboard')
+
+local function divwidth(divby)
+    return math.floor(vim.api.nvim_win_get_width(0) / divby)
+end
+
+local function gettime()
     local total_plugins = #vim.tbl_keys(packer_plugins)
     local datetime = os.date(' %d-%m-%Y   %H:%M:%S')
     local version = vim.version()
@@ -23,10 +30,19 @@ local function footer()
         .. nvim_version_info
 end
 
-local dashboard = require('alpha.themes.dashboard')
+local sections = {}
 
 -- Set header
-dashboard.section.header.val = {
+
+sections.header = {
+    type = 'text',
+    opts = {
+        position = 'center',
+        hl = 'Type',
+    },
+}
+
+sections.header.val = {
     '                                                       ',
     '   ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓  ',
     '   ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒  ',
@@ -41,26 +57,53 @@ dashboard.section.header.val = {
 }
 
 -- Set menu
-dashboard.section.buttons.val = {
-    dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
-    dashboard.button(
+sections.buttons = {
+    type = 'group',
+    opts = {
+        spacing = 1,
+    },
+}
+
+sections.buttons.val = {
+    a_dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
+    a_dashboard.button(
         'f',
         '  > Find file',
-        ':cd $HOME/Workspace | Telescope find_files<CR>'
+        ':Neotree float filesystem toggle=true<CR>'
     ),
-    dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
-    dashboard.button(
+    a_dashboard.button(
         's',
         '  > Settings',
         ':e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>'
     ),
-    dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
+    a_dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
 }
 
-dashboard.section.footer.val = footer()
+sections.mru_cwd = a_startify.section.mru_cwd
+
+sections.time =
+    { type = 'text', val = gettime(), opts = { position = 'center' } }
+
+local config = {
+    layout = {
+        { type = 'padding', val = 2 },
+        sections.header,
+        { type = 'padding', val = 2 },
+        sections.time,
+        { type = 'padding', val = 2 },
+        sections.buttons,
+        { type = 'padding', val = 1 },
+        sections.mru_cwd,
+    },
+    opts = {
+        margin = divwidth(3),
+        redraw_on_resize = true,
+        setup = a_startify.config.opts.setup,
+    },
+}
 
 -- Send config to alpha
-alpha.setup(dashboard.opts)
+alpha.setup(config)
 
 -- Disable folding on alpha buffer
 vim.cmd([[
