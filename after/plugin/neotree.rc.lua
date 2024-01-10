c3bb78a59c08dbc27df68b1ecd8554c8b3144e90
Omar Gallegos <omaraugusto@bego.ai>
2024-01-10 12:23:57 -0600

    feat(neotree): float reveal filesystem with gotkey "|"

--- after/plugin/neotree.rc.lua
+++ after/plugin/neotree.rc.lua
@@ -108,6 +108,6 @@ neotree.setup({
 -- stylua: ignore end
 
 vim.cmd([[nnoremap / :Neotree toggle current reveal_force_cwd<cr>]])
-vim.cmd([[nnoremap \| :Neotree focus filesystem left reveal toggle<cr>]])
+vim.cmd([[nnoremap \| :Neotree focus filesystem float reveal toggle<cr>]])
 vim.cmd([[nnoremap <leader>b :Neotree toggle show buffers right<cr>]])
 vim.cmd([[nnoremap gs :Neotree float git_status<cr>]])
