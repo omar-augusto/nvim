bac58f8176ddaa53342b11d7d2e0c4b16ea60fbf
Omar Gallegos <omaraugusto@bego.ai>
2024-05-05 21:48:56 -0600

    feat: add package providers module

---
+++ lua/config/providers.lua
@@ -0,0 +1,11 @@
+-- Python
+vim.g.python3_host_prog = '/Users/augusto/.pyenv/versions/neovim/bin/python'
+
+-- Ruby
+vim.g.loaded_ruby_provider = 0
+
+-- Node
+vim.g.loaded_node_provider = 0
+
+-- Perl
+vim.g.loaded_perl_provider = 0
