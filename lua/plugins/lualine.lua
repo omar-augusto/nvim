c8f5f587c0e25e3f2f5d30f9d5ef8588a2f6f3f6
Omar Gallegos <omaraugusto@bego.ai>
2024-05-07 20:44:09 -0600

    style: add parenthesis to function calls

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,33 +2,33 @@ local function lualine_config()
     -- Eviline config for lualine
     -- Author: shadmansaleh
     -- Credit: glepnir
-    local lualine = require 'lualine'
+    local lualine = require('lualine')
 
     -- Color table for highlights
     -- stylua: ignore
     local colors = {
-      bg       = '#202328',
-      fg       = '#bbc2cf',
-      yellow   = '#ECBE7B',
-      cyan     = '#008080',
-      darkblue = '#081633',
-      green    = '#98be65',
-      orange   = '#FF8800',
-      violet   = '#a9a1e1',
-      magenta  = '#c678dd',
-      blue     = '#51afef',
-      red      = '#ec5f67',
+        bg       = '#202328',
+        fg       = '#bbc2cf',
+        yellow   = '#ECBE7B',
+        cyan     = '#008080',
+        darkblue = '#081633',
+        green    = '#98be65',
+        orange   = '#FF8800',
+        violet   = '#a9a1e1',
+        magenta  = '#c678dd',
+        blue     = '#51afef',
+        red      = '#ec5f67',
     }
 
     local conditions = {
         buffer_not_empty = function()
-            return vim.fn.empty(vim.fn.expand '%:t') ~= 1
+            return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
         end,
         hide_in_width = function()
             return vim.fn.winwidth(0) > 80
         end,
         check_git_workspace = function()
-            local filepath = vim.fn.expand '%:p:h'
+            local filepath = vim.fn.expand('%:p:h')
             local gitdir = vim.fn.finddir('.git', filepath .. ';')
             return gitdir and #gitdir > 0 and #gitdir < #filepath
         end,
@@ -79,15 +79,15 @@ local function lualine_config()
         table.insert(config.sections.lualine_x, component)
     end
 
-    ins_left {
+    ins_left({
         function()
             return '▊'
         end,
         color = { fg = colors.violet },    -- Sets highlighting of component
         padding = { left = 0, right = 1 }, -- We don't need space before this
-    }
+    })
 
-    ins_left {
+    ins_left({
         -- mode component
         function()
             return ''
@@ -119,25 +119,25 @@ local function lualine_config()
             return { fg = mode_color[vim.fn.mode()] }
         end,
         padding = { right = 1 },
-    }
+    })
 
-    ins_left {
+    ins_left({
         -- filesize component
         'filesize',
         cond = conditions.buffer_not_empty,
-    }
+    })
 
-    ins_left {
+    ins_left({
         'filename',
         cond = conditions.buffer_not_empty,
         color = { fg = colors.magenta, gui = 'bold' },
-    }
+    })
 
-    ins_left { 'location' }
+    ins_left({ 'location' })
 
-    ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+    ins_left({ 'progress', color = { fg = colors.fg, gui = 'bold' } })
 
-    ins_left {
+    ins_left({
         'diagnostics',
         sources = { 'nvim_diagnostic' },
         symbols = { error = ' ', warn = ' ', info = ' ' },
@@ -146,17 +146,17 @@ local function lualine_config()
             color_warn = { fg = colors.yellow },
             color_info = { fg = colors.cyan },
         },
-    }
+    })
 
     -- Insert mid section. You can make any number of sections in neovim :)
     -- for lualine it's any number greater then 2
-    ins_left {
+    ins_left({
         function()
             return '%='
         end,
-    }
+    })
 
-    ins_left {
+    ins_left({
         -- Lsp server name .
         function()
             local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
@@ -174,30 +174,30 @@ local function lualine_config()
             end
         end,
         color = { fg = '#ffffff', gui = 'bold' },
-    }
+    })
 
     -- Add components to right sections
-    ins_right {
-        'o:encoding', -- option component same as &encoding in viml
+    ins_right({
+        'o:encoding',       -- option component same as &encoding in viml
         fmt = string.upper, -- I'm not sure why it's upper case either ;)
         cond = conditions.hide_in_width,
         color = { fg = colors.green, gui = 'bold' },
-    }
+    })
 
-    ins_right {
+    ins_right({
         'fileformat',
         fmt = string.upper,
         icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
         color = { fg = colors.green, gui = 'bold' },
-    }
+    })
 
-    ins_right {
+    ins_right({
         'branch',
         icon = '',
         color = { fg = colors.violet, gui = 'bold' },
-    }
+    })
 
-    ins_right {
+    ins_right({
         'diff',
         -- Is it me or the symbol for modified us really weird
         symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
@@ -207,15 +207,15 @@ local function lualine_config()
             removed = { fg = colors.red },
         },
         cond = conditions.hide_in_width,
-    }
+    })
 
-    ins_right {
+    ins_right({
         function()
             return '▊'
         end,
         color = { fg = colors.violet },
         padding = { left = 1 },
-    }
+    })
 
     -- Now don't forget to initialize lualine
     lualine.setup(config)
